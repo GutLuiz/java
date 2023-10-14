fe6210ad8d96b4f34fbe3af493e0e36324b1a158
Gustavo Lino <137111588+GutLuiz@users.noreply.github.com>
2023-10-13 21:23:17 -0300

    Update and rename Main.java to ExercFixaçaoPT1.java

--- ExercFixaçaoPT1.java
+++ ExercFixaçaoPT1.java
@@ -28,3 +28,22 @@ public class Main {
 		sc.close();
 	}
 }
+package entities;
+
+public class Retangulo {
+		public double width;
+		public double height;
+		
+		
+		public double Area() {
+			return width * height;
+		}
+		public double Perimenter() {
+			return 2 * (width + height);
+		}
+		public double Diagonal() {
+			return Math.sqrt(Math.pow(width,2) + Math.pow(height,2)); 
+		}
+	
+}
+
