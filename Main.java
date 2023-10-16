83aa9b45384339046b0f7003b0ab2c5944065e0d
Gustavo Lino <137111588+GutLuiz@users.noreply.github.com>
2023-10-15 21:02:35 -0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,43 @@
+package curso_programacao;
+import java.util.Locale;
+import java.util.Scanner;
+
+import util.Calculator;
+
+public class Main {
+	
+	
+	public static void main(String[] args) {
+		Locale.setDefault(Locale.US);
+
+		Scanner sc = new Scanner(System.in);
+		/*
+		  MEMBROS ESTATICOS (membros de classe):
+		  =Sao membros que fazem sentido independentemente de objetos. Nao precisam de objetos para serrem chamados.
+		   Sao chamados do proprio nome da classe.
+		  
+		  -APLICAÇOES COMUNS:
+		  = classes utilitarias, exemplo: Math.sqrt(double);
+		  = declaraçao de constantes
+		 */
+		  //exemplos:
+		  //versao 2 (classe Calculator com membros de instancia):
+		
+		Calculator calc = new Calculator();  
+		
+		System.out.println("enter radius: ");
+		double radius = sc.nextDouble();
+		
+		double c = calc.circumference(radius);
+		
+		double v = calc.volume(radius);
+		
+		System.out.printf("circuference: %.2f%n " , c);
+		System.out.printf("volume: %.2f%n " , v);
+		System.out.printf("PI value:  %.2f%n " , calc.PI);
+		
+		
+		
+		sc.close();
+}
+}
