323a98018e78a7d9ec06a2538bb140260c7155d1
Gustavo Lino <137111588+GutLuiz@users.noreply.github.com>
2023-12-02 11:40:55 -0300

    Update and rename Novo Documento de Texto.txt to Main.java

---
+++ HERANÇA/Main.java
@@ -0,0 +1,30 @@
+package curso_programacao;
+
+
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);	
+    	
+      /*
+        ----> HERANÇA <----
+        É um tipo de associaçao que permite que uma classe herde todos dados e comportamentos de outra.
+        
+        = DEFINIÇOES IMPORTANTES:
+        
+        == VATAGENS:
+        -reuso
+        - Polimorfismo
+        
+        ==Sintaxe:
+        - class A extends B
+        
+       */
+       
+    sc.close();
+       
+    }
+}
