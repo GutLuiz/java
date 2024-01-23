1b5993755b6361477fd0e36a543a41c60b12bd1d
Gustavo Lino <137111588+GutLuiz@users.noreply.github.com>
2024-01-23 17:54:58 -0300

    Create SavingsAccount.java

---
+++ UPCASTING E DOWNCASTING/SavingsAccount.java
@@ -0,0 +1,31 @@
+package entities;
+
+public class SavingsAccount  extends Account{
+	private double interestRate;
+	
+	
+	public SavingsAccount() {
+		super();
+	}
+
+
+	public SavingsAccount(Integer number, String name, double balance, double interestRate) {
+		super(number, name, balance);
+		this.interestRate = interestRate;
+	}
+
+
+	public double getInterestRate() {
+		return interestRate;
+	}
+
+
+	public void setInterestRate(double interestRate) {
+		this.interestRate = interestRate;
+	}
+	
+	public void updateBalance() {
+		balance += balance * interestRate;
+	}
+	
+}
