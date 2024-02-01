8c7713009849e8f76b9e28846f4520dae2ad0292
Gustavo Lino <137111588+GutLuiz@users.noreply.github.com>
2024-02-01 19:08:44 -0300

    Create SavingsAccount.java

---
+++ Classe abstrata/SavingsAccount.java
@@ -0,0 +1,37 @@
+package entities;
+
+public class SavingsAccount  extends Account{
+	private double interestRate;
+	
+	
+	public SavingsAccount() {
+		super();
+	}
+
+
+	public SavingsAccount(Integer number, String name, double balance, double interestRate) {
+		super(number, name, balance);
+		this.interestRate = interestRate;
+	}
+
+
+	public double getInterestRate() {
+		return interestRate;
+	}
+
+
+	public void setInterestRate(double interestRate) {
+		this.interestRate = interestRate;
+	}
+	
+	public void updateBalance() {
+		balance += balance * interestRate;
+	}
+	
+
+	@Override
+	public void withdraw(double amount) {
+		balance -= amount;
+	}
+	
+}
