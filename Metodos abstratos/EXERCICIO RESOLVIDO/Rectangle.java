bdda9c855d7c2e4e74edc048293afdbdb51af851
Gustavo Lino <137111588+GutLuiz@users.noreply.github.com>
2024-02-02 18:45:51 -0300

    Create Rectangle.java

---
+++ Metodos abstratos/EXERCICIO RESOLVIDO/Rectangle.java
@@ -0,0 +1,41 @@
+package entities.enums;
+
+import entities.Shape;
+
+public class Rectangle extends Shape{
+		private double width;
+		private double height;
+		
+	public Rectangle() {
+			super();
+		}
+		
+	public Rectangle(Color color, double width, double height) {
+			super(color);
+			this.width = width;
+			this.height = height;
+		}
+	
+	public double getWidth() {
+		return width;
+	}
+
+	public void setWidth(double width) {
+		this.width = width;
+	}
+
+	public double getHeight() {
+		return height;
+	}
+
+	public void setHeight(double height) {
+		this.height = height;
+	}
+
+	@Override
+	public double area() {
+		return width * height;
+	}
+	
+
+}
