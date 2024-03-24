157f83e4d7d32ebb7a818cfa6b438506f313167f
Gustavo Lino <137111588+GutLuiz@users.noreply.github.com>
2024-03-24 19:56:21 -0300

    Create Vehicle.java

---
+++ Interfaces/Exercicio resolvido com interface/Vehicle.java
@@ -0,0 +1,21 @@
+package model;
+
+public class Vehicles {
+	private String model;
+	
+	public Vehicles() {
+	}
+
+	public Vehicles(String model) {
+		this.model = model;
+	}
+
+	public String getModel() {
+		return model;
+	}
+
+	public void setModel(String model) {
+		this.model = model;
+	}
+
+}
