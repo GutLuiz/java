becc46a3c24059161981b20d775a0e7da1d42da8
Gustavo Lino <137111588+GutLuiz@users.noreply.github.com>
2024-03-27 19:03:07 -0300

    Update ContratoServico.java

--- Interfaces/Exercicio fixação/ContratoServico.java
+++ Interfaces/Exercicio fixação/ContratoServico.java
@@ -14,7 +14,18 @@ public class ContratoServico {
 	}
 
 	public void processContract(Contrato contrato, Integer months) {
-		contrato.getInstallment().add(new Installment(LocalDate.of(2018, 7, 25), 206));
-		contrato.getInstallment().add(new Installment(LocalDate.of(2018, 8, 25), 208));
+		double basicQuota = contrato.getTotalValue() / months;// fazendo a divisão do total pela a quantidade de parcelas.
+		
+		for(int i = 1; i <= months; i++) {
+			LocalDate dueDate = contrato.getDate().plusMonths(i);// colocando a data certa das parcelas adicionando mais um mes
+			
+			double interest = online.interest(basicQuota, i);// Trazendo o calculo da taxa
+			
+			double fee = online.paymentFee(basicQuota + interest); // fazendo o calculo trazendo a divisao da parcela de um mes mais a taxa.
+			
+			double quota = basicQuota + interest + fee; // valor total com a taxa e os juros
+			
+			contrato.getInstallment().add(new Installment(dueDate,quota)); // nisso ele instancia na lista e adiciona a data e o valor total.
+		}
 	}
 }
