d50de71ae2ff2fc34e12ff5bd913f775b3bfbff4
Gustavo Lino <137111588+GutLuiz@users.noreply.github.com>
2024-04-10 11:40:06 -0300

    Create Motivador2Emp.java

---
+++ Interfaces/Interface comparable/Motivador2Emp.java
@@ -0,0 +1,32 @@
+package entities;
+
+public class Employee implements Comparable<Employee>{
+    private String name;
+    private double salary;
+
+    public Employee(String name, double salary) {
+        this.name = name;
+        this.salary = salary;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+
+    @Override
+    public int compareTo(Employee orther) { // serve para comparar um objeto com outro.
+        return name.compareTo(orther.name);
+    }
+}
