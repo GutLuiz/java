4d2638c683b275bd52be46eec9420ff39f5d1b53
Gustavo Lino <137111588+GutLuiz@users.noreply.github.com>
2024-04-10 18:48:06 -0300

    Create BrazilInterestService.java

---
+++ Interfaces/Default methods/BrazilInterestService.java
@@ -0,0 +1,16 @@
+package entities;
+
+import java.security.InvalidParameterException;
+
+public class UsaInterestService implements InterestService{
+    private double interestRate;
+
+    public UsaInterestService(double interestRate) {
+        this.interestRate = interestRate;
+    }
+
+    @Override
+    public double getInterestRate() {
+        return interestRate;
+    }
+}
